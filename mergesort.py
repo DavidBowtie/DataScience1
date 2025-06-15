ad4f28c936908d31f33c35281e4a58c6f18f3e78
David Höhn <davidjonah.hoehn@stud.uni-goettingen.de>
2025-06-15 18:14:42 +0200

    mergesort.py bereinigt

--- mergesort.py
+++ mergesort.py
@@ -1,45 +1,44 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        l = 0
-        r = 0
-        i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
-
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
-
-
+#Bereinigung 1: Imports an den Anfang
 import matplotlib.pyplot as plt
+
+#Bereinigung 2: list_to_sort_by_merge in arr umbenannt (zu)
+def mergeSort(arr):
+    #Bereinigung 3: IF Bedingung vereinfacht
+    if len(arr) > 1:
+        #Bereinigung 4: Bessere Variablennamen  (nicht mid, left, right)
+        mid_index = len(arr) // 2
+        left_half = arr[:mid_index]
+        right_half = arr[mid_index:]
+
+        mergeSort(left_half)
+        mergeSort(right_half)
+
+        #Bereinigung 5: Bessere Variablennamen (nicht l, r, i)
+        left_index = 0
+        right_index = 0
+        main_index = 0
+
+        while left_index < len(left_half) and right_index < len(right_half):
+            if left_half[left_index] <= right_half[right_index]:
+                #Bereinigung 6: Unnötige ASSIGNMENT-Methode entfernt
+                arr[main_index] = left_half[left_index]
+                left_index += 1
+            else:
+                #Bereinigung 7: Unnötige ASSIGNMENT-Methode entfernt
+                arr[main_index] = right_half[right_index]
+                right_index += 1
+            main_index += 1
+
+        while left_index < len(left_half):
+            arr[main_index] = left_half[left_index]
+            left_index += 1
+            main_index += 1
+
+        while right_index < len(right_half):
+            arr[main_index] = right_half[right_index]
+            right_index += 1
+            main_index += 1
+
 
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
