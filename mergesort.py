65e12ac8c65811d4392650e7009c48781539453b
David Höhn <davidjonah.hoehn@stud.uni-goettingen.de>
2025-06-15 18:47:37 +0200

    mergesort.py plotting-teil überarbeitet

--- mergesort.py
+++ mergesort.py
@@ -1,30 +1,33 @@
-#Bereinigung 1: Imports an den Anfang
+# Bereinigung 1: Imports an den Anfang
 import matplotlib.pyplot as plt
 
-#Bereinigung 2: list_to_sort_by_merge in arr umbenannt (zu)
-def mergeSort(arr):
-    #Bereinigung 3: IF Bedingung vereinfacht
+# Bereinigung 2: list_to_sort_by_merge in arr umbenannt (zu)
+# Bereinigung 3: mergeSort in merge_sort umbenannt (in snake_case nach PEP8)
+def merge_sort(arr):
+    # Bereinigung 4: Docstring hinzugefügt
+    """Sortiert eine Liste mit dem Mergesort-Algorithmus"""
+    # Bereinigung 5: IF Bedingung vereinfacht
     if len(arr) > 1:
-        #Bereinigung 4: Bessere Variablennamen  (nicht mid, left, right)
+        # Bereinigung 6: Bessere Variablennamen  (nicht mid, left, right)
         mid_index = len(arr) // 2
         left_half = arr[:mid_index]
         right_half = arr[mid_index:]
 
-        mergeSort(left_half)
-        mergeSort(right_half)
+        merge_sort(left_half)
+        merge_sort(right_half)
 
-        #Bereinigung 5: Bessere Variablennamen (nicht l, r, i)
+        # Bereinigung 7: Bessere Variablennamen (nicht l, r, i)
         left_index = 0
         right_index = 0
         main_index = 0
 
         while left_index < len(left_half) and right_index < len(right_half):
             if left_half[left_index] <= right_half[right_index]:
-                #Bereinigung 6: Unnötige ASSIGNMENT-Methode entfernt
+                # Bereinigung 8: Unnötige ASSIGNMENT-Methode entfernt
                 arr[main_index] = left_half[left_index]
                 left_index += 1
             else:
-                #Bereinigung 7: Unnötige ASSIGNMENT-Methode entfernt
+                # Bereinigung 9: Unnötige ASSIGNMENT-Methode entfernt
                 arr[main_index] = right_half[right_index]
                 right_index += 1
             main_index += 1
@@ -40,11 +43,28 @@ def mergeSort(arr):
             main_index += 1
 
 
+# Plotting
+
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+original_list = list(my_list) 
+
+plt.figure(figsize=(12, 5))
+
+# Plot 1: Liste vor Sortierung
+plt.subplot(1, 2, 1)
+plt.bar(range(len(my_list)), original_list, color='skyblue')
+plt.title("Vorher")
+plt.xlabel("Index")
+plt.ylabel("Wert")
+
+merge_sort(my_list) # Sortiere die Liste
+
+# Plot 2: Liste nach Sortierung
+plt.subplot(1, 2, 2)
+plt.bar(range(len(my_list)), my_list, color='lightcoral')
+plt.title("Nachher")
+plt.xlabel("Index")
+plt.ylabel("Wert")
+
+plt.tight_layout() # Passt Layout an
+plt.show() # Zeigt die Plots an
